5e00c3ed18afe4e391bc8f9ef716465136ad4294
Mohamed Diaa <mohammeddiaato@gmail.com>
2026-08-13 18:25:23 +0300

    implementing Task2

--- Assignment5/Task2.c
+++ Assignment5/Task2.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 
-
 int main()
 {
-int arr[10];
-int sum=0;
-for(int i=0;i<10;i++)
-{
-	printf("Enter Number%d: ",i+1);scanf("%d",&arr[i]);
-	sum+=arr[i];
-}
+	int arr[10];
+	int SumOdd = 0;
+	int SumEven = 0;
+	for (int i = 0; i < 10; i++)
+	{
+		printf("Enter Number%d: ", i + 1);
+		scanf("%d", &arr[i]);
+		if (arr[i] % 2)
+		{
+			SumOdd += arr[i];
+		}
+		else
+		{
 
-// for(int i=9;i>=0;i--)
-// {
-	printf("Summion is %d\n",sum);
-	printf("Average is %f\n",sum/10.0);
-// }
-    return 0;
-    
+			SumEven += arr[i];
+		}
+	}
+
+	printf("Odd Summion is %d\n", SumOdd);
+	printf("Even Summion is %d\n", SumEven);
+
+	return 0;
 }
