063434058f2ef2398806431c2e40b77acc383b56
Mohamed Diaa <mohammeddiaato@gmail.com>
2026-08-13 17:49:50 +0300

    Solving Assignment4 and making a script for compiling

---
+++ Assignment4/Task2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int Factorial(int val)
+{
+    int res=1;
+    for(int i=1;i<=val;i++)
+    {
+        res*=i;
+    }
+    return res;
+} 
+int nCr_Evaluation(int n,int r)
+{
+    if(n>=r)
+  {
+      return Factorial(n)/(Factorial(r)*Factorial(n-r));
+      
+    }else 
+    {
+        printf(" r must be <=n!\n");
+      return 0;
+  }
+} 
+
+
+int main()
+{
+    int n,r;
+    printf("Enter n: ");scanf("%d",&n);
+    printf("Enter r: ");scanf("%d",&r);
+    printf("(%d)P(%d) evaluation is: %d\n",n,r,nCr_Evaluation(n,r));
+
+}
