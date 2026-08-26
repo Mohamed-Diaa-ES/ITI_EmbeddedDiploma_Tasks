376fbc8e036e12f94e8f6d9c668cdc0a78c5f810
Mohamed Diaa <mohammeddiaato@gmail.com>
2026-08-26 07:28:10 +0300

    The assignment works

--- EmbeddedDriversCodingAssignment/HAL/LCD/LCD_Config.h
+++ EmbeddedDriversCodingAssignment/HAL/LCD/LCD_Config.h
@@ -10,9 +10,9 @@
 #define LCD_DataPins_Group  DIO_PORTA
 
 #define LCD_ENPin_Number    DIO_PIN0
-#define LCD_ENPin_Group     DIO_PORTC
+#define LCD_ENPin_Group     DIO_PORTB
 
 #define LCD_RSPin_Number    DIO_PIN1
-#define LCD_RSPin_Group     DIO_PORTC
+#define LCD_RSPin_Group     DIO_PORTB
 
 #endif
