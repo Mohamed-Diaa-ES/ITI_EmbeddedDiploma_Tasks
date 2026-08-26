54c3b2735502d2493c21c570ca89f5d378627038
Mohamed Diaa <mohammeddiaato@gmail.com>
2026-08-26 07:10:11 +0300

    adding the Sisnsoidal Form

--- EmbeddedDriversCodingAssignment/main.c
+++ EmbeddedDriversCodingAssignment/main.c
@@ -1,13 +1,18 @@
 
 #include "MCAL/DIO/DIO_Interface.h"
 #include "HAL/7SegDriver/Seven_Seg_Interface.h"
+#include "HAL/LCD/LCD_Interface.h"
 #include <util/delay.h>
 
 int main()
 {
-    SSD_voidInitialDataPort();
-    SSD_voidEnable();
-    SSD_voidSendNumber(5);
+    LCD_voidInit();
+    u8 stringTobeSent[20]="MohammedDiaa";
+    for(int i=0;stringTobeSent[i]!='\0';i++)
+    {
+        LCD_voidSendData(stringTobeSent[i]);
+        LCD_voidSendData('_');
+    }
     while (1)
     {
 
