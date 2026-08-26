7368e3353f39110283f1b086debea28a6010e567
Mohamed Diaa <mohammeddiaato@gmail.com>
2026-08-26 07:42:17 +0300

    Assign ment 2

--- EmbeddedDriversCodingAssignment/main.c
+++ EmbeddedDriversCodingAssignment/main.c
@@ -1,20 +1,48 @@
-
 #include "MCAL/DIO/DIO_Interface.h"
-// #include "HAL/7SegDriver/Seven_Seg_Interface.h"
 #include "HAL/LCD/LCD_Interface.h"
 #include <util/delay.h>
 
-int main()
+int main(void)
 {
+   
+      //  UpperCase 'M'
+    const u8 UpperCase_M[8] = {
+        0b10001,  
+        0b11011,  
+        0b10101, 
+        0b10001,  
+        0b10001,  
+        0b10001,  
+        0b10001,  
+        0b00000   
+    };
+
+    //  lowercase 'm'
+    const u8 LowerCase_m[8] = {
+        0b00000,  
+        0b00000,  
+        0b11010,  
+        0b10101, 
+        0b10101, 
+        0b10101,  
+        0b10101, 
+        0b00000   
+    };
+
+
     LCD_voidInit();
-    u8 stringTobeSent[20]="MohammedDiaa";
-    for(int i=0;stringTobeSent[i]!='\0';i++)
-    {
-        LCD_voidSendData(stringTobeSent[i]);
-        LCD_voidSendData('_');
-    }
+
+    //first block  first row first column
+    LCD_voidSendSpecialCharecter(0, UpperCase_M, 0, 0);
+    
+    
+    //Second block  Second row first column
+    LCD_voidSendSpecialCharecter(1, LowerCase_m, 0, 1);
+
     while (1)
     {
-
+        // Infinite loop to keep the microcontroller running
     }
+    
+    return 0;
 }
