53fa5b6ca69d4aed7a4298179b100964f1a88315
Mohamed Diaa <mohammeddiaato@gmail.com>
2026-08-28 02:48:57 +0300

    adding the labs work on the way

--- EmbeddedDriversCodingAssignment/HAL/LCD/LCD_Config.h
+++ EmbeddedDriversCodingAssignment/HAL/LCD/LCD_Config.h
@@ -7,12 +7,12 @@
 #ifndef LCD_CONFIG_H
 #define LCD_CONFIG_H
 
-#define LCD_DataPins_Group  DIO_PORTA
+#define LCD_DataPins_Group  DIO_PORTB
 
 #define LCD_ENPin_Number    DIO_PIN0
-#define LCD_ENPin_Group     DIO_PORTB
+#define LCD_ENPin_Group     DIO_PORTC
 
 #define LCD_RSPin_Number    DIO_PIN1
-#define LCD_RSPin_Group     DIO_PORTB
+#define LCD_RSPin_Group     DIO_PORTC
 
 #endif
--- EmbeddedDriversCodingAssignment/main.c
+++ EmbeddedDriversCodingAssignment/main.c
@@ -5,21 +5,29 @@
 #include "HAL/7SegDriver/Seven_Seg_Interface.h"
 int main(void)
 {
-   LCD_voidInit();
 
-   KPD_voidInit();
-   u8 val = KPD_u8GetKey();
-   
-   SSD_voidInitialDataPort();
-   while (1)
-   {
-        val = KPD_u8GetKey();
-        if (val!=0)
+    // setting two pins to control the switch
+    DIO_voidSetPinDirection(DIO_PORTA, DIO_PIN0, DIO_OUTPUT);
+    DIO_voidSetPinValue(DIO_PORTA, DIO_PIN0, DIO_HIGH);
+
+    // setting pull up resistor to get the switch is pressed or not
+    DIO_voidSetPinDirection(DIO_PORTD, DIO_PIN0, DIO_INPUT);
+    DIO_voidSetPinValue(DIO_PORTD, DIO_PIN0, DIO_HIGH);
+    u8 PinPressed = 1;
+    DIO_voidGetPinValue(DIO_PORTD, DIO_PIN0, &PinPressed);
+    LCD_voidInit();
+    LCD_voidSendData('5');
+    while (1)
+    {
+        DIO_voidGetPinValue(DIO_PORTD, DIO_PIN0, &PinPressed);
+
+        while (PinPressed == 0)
         {
-            SSD_voidSendNumber(val-'0');
+            DIO_voidSetPinValue(DIO_PORTA, DIO_PIN0, DIO_LOW);
+            DIO_voidGetPinValue(DIO_PORTD, DIO_PIN0, &PinPressed);
         }
-        
+        DIO_voidSetPinValue(DIO_PORTA, DIO_PIN0, DIO_HIGH);
     }
-    
+
     return 0;
 }
