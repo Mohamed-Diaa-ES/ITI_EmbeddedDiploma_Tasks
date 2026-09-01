e392fbda877c1993d1009b551e8380feebd3e5d6
Mohammed diaa <mahmmedy2016@gmail.com>
2026-09-01 11:27:08 +0300

    Wrting the Interface file

--- EmbeddedDriversCodingAssignment/MCAL/Timers/Timer_Interface.h
+++ EmbeddedDriversCodingAssignment/MCAL/Timers/Timer_Interface.h
@@ -6,5 +6,9 @@
 #ifndef _Timer_Interface_H
 #define _Timer_Interface_H
 
+u8 Timer0_u8Init();
+u8 Timer0_u8_my_delay_ms(u16 ms);
+u8 Timer0_u8SetCompareValue(u16 ms);
+void Timer0_voidSet_OVE_CallBack(void(*Timer0_CallBack));
 
 #endif
--- EmbeddedDriversCodingAssignment/MCAL/Timers/Timer_Program.c
+++ EmbeddedDriversCodingAssignment/MCAL/Timers/Timer_Program.c
@@ -4,3 +4,8 @@
  * @author  Mohammed Diaa <mohammeddiaato@gmail.com>
  */
 
+
+u8 Timer0_u8Init();
+u8 Timer0_u8_my_delay_ms(u16 ms);
+u8 Timer0_u8SetCompareValue(u16 ms);
+void Timer0_voidSet_OVE_CallBack(void(*Timer0_CallBack));
