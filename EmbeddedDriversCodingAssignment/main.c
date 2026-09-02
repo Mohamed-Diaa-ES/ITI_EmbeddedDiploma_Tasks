77da16f88ac59ace9b06c6e2b4500261bb964e96
Mohamed Diaa <mohammeddiaato@gmail.com>
2026-09-02 05:37:49 +0300

    doing the task of the electronic switches

--- EmbeddedDriversCodingAssignment/main.c
+++ EmbeddedDriversCodingAssignment/main.c
@@ -7,59 +7,36 @@
 int main(void)
 {
 
-    Timer0_u8Init();
-    ADC_voidInit();
-
+    DIO_voidSetPinDirection(DIO_PORTA, DIO_PIN0, DIO_OUTPUT);
     DIO_voidSetPinDirection(DIO_PORTA, DIO_PIN1, DIO_OUTPUT);
-    DIO_voidSetPinDirection(DIO_PORTA, DIO_PIN0, DIO_INPUT);
+    DIO_voidSetPinDirection(DIO_PORTD, DIO_PIN0, DIO_INPUT);
+    DIO_voidSetPinValue(DIO_PORTD, DIO_PIN0, DIO_HIGH);
 
-    f32 voltage_result = 0;
-    f32 OneQuarter = 5 / 4;
+    DIO_voidSetPinValue(DIO_PORTA, DIO_PIN0, DIO_LOW);
+    DIO_voidSetPinValue(DIO_PORTA, DIO_PIN1, DIO_HIGH);
 
-    u16 adcReading = 0;
-
+    u8 val = 1;
     while (1)
     {
-        DIO_voidSetPinValue(DIO_PORTA, DIO_PIN1, DIO_HIGH);
-        ADC_u8ConvertSynch(1, &adcReading);
-        voltage_result = adcReading * 5. / 1024;
-
-        if (voltage_result >= 5)
+        DIO_voidGetPinValue(DIO_PORTD, DIO_PIN0, &val);
+        if (val == 0)
         {
-            Timer0_u8_my_delay_ms(50*1);
+            _delay_ms(20);
+            DIO_voidGetPinValue(DIO_PORTD, DIO_PIN0, &val);
+            if (val == 0)
+            {
+                // Turn RL1 OFF first, then turn RL3 ON
+                DIO_voidSetPinValue(DIO_PORTA, DIO_PIN1, DIO_LOW);
+                _delay_ms(10);
+                DIO_voidSetPinValue(DIO_PORTA, DIO_PIN0, DIO_HIGH);
+            }
         }
-        else if (voltage_result >OneQuarter*3)
+        else
         {
-            Timer0_u8_my_delay_ms(50*2);
-        }
-      
-        else if (voltage_result > OneQuarter*2)
-        {
-            Timer0_u8_my_delay_ms(50*3);
-        }else
-        {
-            Timer0_u8_my_delay_ms(50*4);
-
-        }
-      
-
-            DIO_voidSetPinValue(DIO_PORTA, DIO_PIN1, DIO_LOW);
-                    if (voltage_result >= 5)
-        {
-            Timer0_u8_my_delay_ms(50*1);
-        }
-        else if (voltage_result >OneQuarter*3)
-        {
-            Timer0_u8_my_delay_ms(50*2);
-        }
-      
-        else if (voltage_result > OneQuarter*2)
-        {
-            Timer0_u8_my_delay_ms(50*3);
-        }else
-        {
-            Timer0_u8_my_delay_ms(50*4);
-
+            // Turn RL3 OFF first, then turn RL1 ON
+            DIO_voidSetPinValue(DIO_PORTA, DIO_PIN0, DIO_LOW);
+            _delay_ms(10);
+            DIO_voidSetPinValue(DIO_PORTA, DIO_PIN1, DIO_HIGH);
         }
     }
 
