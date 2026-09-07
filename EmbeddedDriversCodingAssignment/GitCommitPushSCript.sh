bd7b2bf271b7afd2eb7a8b187d538bc25261d427
Mohamed Diaa <mohammeddiaato@gmail.com>
2026-09-07 23:22:57 +0300

    Exti Assignment

--- EmbeddedDriversCodingAssignment/GitCommitPushSCript.sh
+++ EmbeddedDriversCodingAssignment/GitCommitPushSCript.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-git pull origin main
 read Commitmessage
 git add .
 git commit -m"$Commitmessage"
--- EmbeddedDriversCodingAssignment/main.c
+++ EmbeddedDriversCodingAssignment/main.c
@@ -8,50 +8,35 @@
 #include "MCAL/GIE/GIE_Interface.h"
 #include "MCAL/EXTI/EXTI_Interface.h"
 
-#define SPCR_Reg    *((volatile u8*)0x2D)
-#define SPDR_Reg    *((volatile u8*)0x2F)
-#define SPSR_Reg    *((volatile u8*)0x2E)
-void SPI_MasterInit(void)
-{
-/* Set MOSI and SCK output, all others input */
-    DIO_voidSetPortDirection(DIO_PORTA,0xA0);
-/* Enable SPI, Master, set clock rate fck/16 */
-SPCR_Reg = (1<<6)|(1<<4)|(1<<0);
-}
-void SPI_MasterTransmit(char cData)
-{
-/* Start transmission */
-SPDR_Reg = cData;
-/* Wait for transmission complete */
-while(!(SPSR_Reg & (1<<7)))
-;
-}
-
 void TogglePin()
 {
-    TOG_BIT(DIO_u8_DDRB_REG,DIO_PIN0);
+    static volatile int count = 0;
 
+    if ((count++) % 2)
+    {
+        DIO_voidSetPinValue(DIO_PORTB, DIO_PIN1, DIO_LOW);
+        DIO_voidSetPinValue(DIO_PORTB, DIO_PIN0, DIO_HIGH);
+    }else
+    {
+        DIO_voidSetPinValue(DIO_PORTB, DIO_PIN0, DIO_LOW);
+        DIO_voidSetPinValue(DIO_PORTB, DIO_PIN1, DIO_HIGH);
+    }
 }
 
 int main(void)
 {
-    
+
     DIO_voidSetPinDirection(DIO_PORTB, DIO_PIN0, DIO_OUTPUT);
-    DIO_voidSetPinValue(DIO_PORTB, DIO_PIN0, DIO_HIGH);
+    DIO_voidSetPinDirection(DIO_PORTB, DIO_PIN1, DIO_OUTPUT);
+    DIO_voidSetPinValue(DIO_PORTB, DIO_PIN0, DIO_LOW);
+    DIO_voidSetPinValue(DIO_PORTB, DIO_PIN1, DIO_HIGH);
 
+    EXTI_voidSetCallBackFunction(EXTI_0, TogglePin);
+    EXTI_voidInit(EXTI_0, Rising_Edge1_0);
+    DIO_voidSetPinValue(EXTI0_DIO_Port, EXTI0_DIO_Pin, DIO_HIGH);
 
-    EXTI_voidSetCallBackFunction(EXTI_0,TogglePin);
-    EXTI_voidInit(EXTI_0,Failling_Edge1_0);
-
-
-
-    SPI_MasterInit();
-    LCD_voidInit();
-    u8 Result=0;
     while (1)
     {
-        SPI_MasterTransmit('5');
-        
     }
 
     return 0;
