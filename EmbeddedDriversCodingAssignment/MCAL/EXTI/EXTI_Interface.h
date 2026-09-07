0168572dd581c97fc40b5a6560e5e49a2e8c7268
Mohamed Diaa <mohammeddiaato@gmail.com>
2026-09-07 08:20:24 +0300

    ""[Dadding the EXTI_DriverToWork

--- EmbeddedDriversCodingAssignment/MCAL/EXTI/EXTI_Interface.h
+++ EmbeddedDriversCodingAssignment/MCAL/EXTI/EXTI_Interface.h
@@ -3,15 +3,34 @@
  * @brief   This shows the interface and the APIs of the EXTI that can Sense the signals
  * @author  Mohammed Diaa <mohammeddiaato@gmail.com>
  */
+
 #ifndef _EXTI_Interface_H
 #define _EXTI_Interface_H
 
 typedef enum
 {
+    EXTI_0,
+    EXTI_1,
+    EXTI_2
+} EXTI_Numbers_t;
 
-}EXTI_Numbers_t;
+#define EXTI0_DIO_Pin   DIO_PIN2
 
+#define EXTI1_DIO_Pin   DIO_PIN3
 
-void EXTI_voidInit(u8 ,u8);
+#define EXTI2_DIO_Pin   DIO_PIN2
+
+#define EXTI0_DIO_Port  DIO_PORTD
+
+#define EXTI1_DIO_Port  DIO_PORTD
+
+#define EXTI2_DIO_Port  DIO_PORTB
+
+void EXTI_voidInit(EXTI_Numbers_t EXTI_Number, u8 SourceControl);
+void EXTI_Enable(EXTI_Numbers_t EXTI_Number);
+void EXTI_Disable(EXTI_Numbers_t EXTI_Number);
+
+inline void EXTI_voidChangeSourceControl(EXTI_Numbers_t EXTI_Number, u8 SourceControl);
+void EXTI_voidSetCallBackFunction(EXTI_Numbers_t EXTI_Number, void (*EXTI_CallBack)(void));
 
 #endif
--- EmbeddedDriversCodingAssignment/MCAL/EXTI/EXTI_Program.c
+++ EmbeddedDriversCodingAssignment/MCAL/EXTI/EXTI_Program.c
@@ -11,3 +11,134 @@
 #include "EXTI_Interface.h"
 #include "EXTI_Private.h"
 
+static void (*EXTI0_CallBack_GlobalSet)(void) = NULL;
+static void (*EXTI1_CallBack_GlobalSet)(void) = NULL;
+static void (*EXTI2_CallBack_GlobalSet)(void) = NULL;
+
+void EXTI_voidInit(EXTI_Numbers_t EXTI_Number, u8 SourceControl)
+{
+    if (SourceControl > Rising_Edge1_0 || (EXTI_Number == EXTI_2 && SourceControl > Rising_Edge2))
+        return;
+    switch (EXTI_Number)
+    {
+    case EXTI_0:
+        DIO_voidSetPinDirection(EXTI0_DIO_Port, EXTI0_DIO_Pin, DIO_INPUT);
+        MCUCR_Reg = (MCUCR_Reg & EXTI0_SenseControl_ClearingMask) | (SourceControl<<ISC00);
+        break;
+    case EXTI_1:
+        DIO_voidSetPinDirection(EXTI1_DIO_Port, EXTI1_DIO_Pin, DIO_INPUT);
+        MCUCR_Reg = (MCUCR_Reg & EXTI1_SenseControl_ClearingMask) | (SourceControl<<ISC10);
+        break;
+    case EXTI_2:
+        DIO_voidSetPinDirection(EXTI2_DIO_Port, EXTI2_DIO_Pin, DIO_INPUT);
+        MCUCSR_Reg = (MCUCSR_Reg & EXTI2_SenseControl_ClearingMask) | (SourceControl<<ISC2);
+        break;
+    default:
+        return;
+        break;
+    }
+    if (!GET_BIT(SREG_Reg,Interrupt_Pin))
+    {
+            GIE_Enable();
+    }
+    
+
+}
+void EXTI_Enable(EXTI_Numbers_t EXTI_Number)
+{
+
+    switch (EXTI_Number)
+    {
+    case EXTI_0:
+        SET_BIT(GICR_Reg, INT0);
+        break;
+    case EXTI_1:
+        SET_BIT(GICR_Reg, INT1);
+        break;
+    case EXTI_2:
+        SET_BIT(GICR_Reg, INT2);
+        break;
+    }
+}
+void EXTI_Disable(EXTI_Numbers_t EXTI_Number)
+{
+
+    switch (EXTI_Number)
+    {
+    case EXTI_0:
+        CLR_BIT(GICR_Reg, INT0);
+        break;
+    case EXTI_1:
+        CLR_BIT(GICR_Reg, INT1);
+        break;
+    case EXTI_2:
+        CLR_BIT(GICR_Reg, INT2);
+        break;
+    }
+}
+
+inline void EXTI_voidChangeSourceControl(EXTI_Numbers_t EXTI_Number, u8 SourceControl)
+{
+    EXTI_voidInit(EXTI_Number, SourceControl);
+}
+void EXTI_voidSetCallBackFunction(EXTI_Numbers_t EXTI_Number, void (*EXTI_CallBack)(void))
+{
+
+    switch (EXTI_Number)
+    {
+    case EXTI_0:
+        EXTI0_CallBack_GlobalSet = EXTI_CallBack;
+        break;
+    case EXTI_1:
+        EXTI1_CallBack_GlobalSet = EXTI_CallBack;
+        break;
+    case EXTI_2:
+        EXTI2_CallBack_GlobalSet = EXTI_CallBack;
+        break;
+    }
+}
+/**
+ * ISR of EXTI 0
+ */
+void __vector_1(void) __attribute__((signal));
+void __vector_1(void)
+{
+    if (EXTI0_CallBack_GlobalSet != NULL)
+    {
+        EXTI0_CallBack_GlobalSet();
+    }
+    else
+    {
+        return;
+    }
+}
+/**
+ * ISR of EXTI 1
+ */
+void __vector_2(void) __attribute__((signal));
+void __vector_2(void)
+{
+    if (EXTI1_CallBack_GlobalSet != NULL)
+    {
+        EXTI1_CallBack_GlobalSet();
+    }
+    else
+    {
+        return;
+    }
+}
+/**
+ * ISR of EXTI 2
+ */
+void __vector_3(void) __attribute__((signal));
+void __vector_3(void)
+{
+    if (EXTI2_CallBack_GlobalSet != NULL)
+    {
+        EXTI2_CallBack_GlobalSet();
+    }
+    else
+    {
+        return;
+    }
+}
