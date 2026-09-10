e2b91504c230866f0cde030d50edd95f77cfce90
Mohamed Diaa <mohammeddiaato@gmail.com>
2026-09-10 12:53:37 +0300

    Creating the LCD TASKs of the FREERTOS

--- EmbeddedDriversCodingAssignment/main.c
+++ EmbeddedDriversCodingAssignment/main.c
@@ -2,52 +2,48 @@
 #include "FreeRTOS/FreeRTOS.h"
 #include "FreeRTOS/task.h"
 #include "MCAL/DIO/DIO_Interface.h"
+#include "HAL/LCD/LCD_Interface.h"
+#include "MCAL/GIE/GIE_Interface.h"
+#include "HAL/LCD/LCD_Private.h"
 #include "MCAL/DIO/DIO_Private.h"
 #include "LIB/BIT_MATH.h"
 
-void ToggFunc1()
+void LCD1()
+{
+    u8 StringToSend2[] = "Hello I am task1";
+
+    while (1)
+    {
+        {
+
+            LCD_voidSendString(StringToSend2);
+            vTaskDelay(100);   
+        }
+    }
+}
+void LCD2()
 {
 
-    DIO_voidSetPinDirection(DIO_PORTA, DIO_PIN1, DIO_OUTPUT);
-    DIO_voidSetPinValue(DIO_PORTA, DIO_PIN1, DIO_HIGH);
+    u8 StringToSend2[] = "Hello I am task2";
     while (1)
     {
-        vTaskDelay(500);
-        TOG_BIT(DIO_u8_PORTA_REG, DIO_PIN1);
+        
+        LCD_voidSendString(StringToSend2);
+        vTaskDelay(100);
     }
 }
-void ToggFunc2()
-{
 
-    DIO_voidSetPinDirection(DIO_PORTA, DIO_PIN2, DIO_OUTPUT);
-    DIO_voidSetPinValue(DIO_PORTA, DIO_PIN2, DIO_HIGH);
-    while (1)
-    {
-        vTaskDelay(1000);
-        TOG_BIT(DIO_u8_PORTA_REG, DIO_PIN2);
-    }
-}
-void ToggFunc3()
-{
-    DIO_voidSetPinDirection(DIO_PORTA, DIO_PIN3, DIO_OUTPUT);
-    DIO_voidSetPinValue(DIO_PORTA, DIO_PIN3, DIO_HIGH);
-    while (1)
-    {
-        vTaskDelay(2000);
-        TOG_BIT(DIO_u8_PORTA_REG, DIO_PIN3);
-    }
-}
 #define StackSize 400
 int main(void)
 {
-
-    xTaskCreate(&ToggFunc1, NULL, StackSize, NULL, 4, NULL);
-    xTaskCreate(&ToggFunc2, NULL, StackSize, NULL, 4, NULL);
-    xTaskCreate(&ToggFunc3, NULL, StackSize, NULL, 4, NULL);
+    GIE_Enable();
+    LCD_voidInit();
+    xTaskCreate(&LCD1, NULL, StackSize, NULL, 4, NULL);
+    xTaskCreate(&LCD2, NULL, StackSize, NULL, 4, NULL);
 
     vTaskStartScheduler();
-    DIO_voidSetPinDirection(DIO_PORTA, DIO_PIN4, DIO_OUTPUT);
-    DIO_voidSetPinValue(DIO_PORTA, DIO_PIN4, DIO_HIGH);
-    while(1);
+
+    while (1)
+        ;
     return 0;
 }
